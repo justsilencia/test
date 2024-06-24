00176d567b5bcfe5e387f7155be35b737f1649e7
justsilencia <legalpet@protonmail.com>
2024-06-24 12:54:07 -0400

    Function.

--- functions/badFunc.js
+++ functions/badFunc.js
@@ -1,29 +1,14 @@
-// import express, { Router } from "express";
-// import serverless from "serverless-http";
+import express, { Router } from "express";
+import serverless from "serverless-http";
 
-// export async function handler(event, context) {
-//     const api = express();
-//     const router = Router();
+export async function handler(event, context) {
+    const api = express();
+    const router = Router();
 
-//     router.get('/fulfilled', (req, res) => res.status(200).json({ "work": "Lets go"}));
+    router.get('/fulfilled', (req, res) => res.status(200).json({ "work": "Lets go"}));
 
-//     api.use('/api/', router);
+    api.use('/api/', router);
   
-//     return serverless(api)(event, context);
-//   }
+    return serverless(api)(event, context);
+  }
 
-const express = require('express');
-const serverless = require('serverless-http');
-
-const app = express();
-const router = express.Router();
-
-router.get('/fulfilled', (req, res) => {
-  res.status(200).json({ work: "Let's go" });
-});
-
-app.use('/api', router);
-
-module.exports.handler = serverless(app);
-
-  
