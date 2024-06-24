3ee3c521092bb25eb3042bdff9d7a11cd956cea5
justsilencia <legalpet@protonmail.com>
2024-06-24 12:41:31 -0400

    Function.

--- functions/badFunc.js
+++ functions/badFunc.js
@@ -5,6 +5,11 @@ export async function handler(event, context) {
     const api = express();
     const router = Router();
   
+    router.get('/', (req, res) => {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write('<h1>Hello from Express.js!</h1>');
+      res.end();
+    });
     router.get('/fulfilled', (req, res) => res.status(200).json({ "work": "Lets go"}));
   
     api.use(json());
