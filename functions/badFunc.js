d32af3712a2cfe12d7640616aaae736b8852aa22
justsilencia <legalpet@protonmail.com>
2024-06-24 12:46:46 -0400

    Function.

--- functions/badFunc.js
+++ functions/badFunc.js
@@ -1,18 +1,12 @@
-import express, { Router, json } from "express";
+import express, { Router } from "express";
 import serverless from "serverless-http";
 
 export async function handler(event, context) {
     const api = express();
     const router = Router();
-  
-    router.get('/', (req, res) => {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write('<h1>Hello from Express.js!</h1>');
-      res.end();
-    });
+    
     router.get('/fulfilled', (req, res) => res.status(200).json({ "work": "Lets go"}));
-  
-    api.use(json());
+
     api.use('/api/', router);
   
     return serverless(api)(event, context);
