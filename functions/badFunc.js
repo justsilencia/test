23c92f8940421150c11551dca975889073574088
justsilencia <legalpet@protonmail.com>
2024-06-25 08:50:03 -0400

    Function.

--- functions/badFunc.js
+++ functions/badFunc.js
@@ -1,14 +1,27 @@
+// import express, { Router } from "express";
+// import serverless from "serverless-http";
+
+// export async function handler(event, context) {
+//     const api = express();
+//     const router = Router();
+
+//     router.get('/fulfilled', (req, res) => res.status(200).json({ "work": "Lets go"}));
+
+//     api.use('/api/', router);
+  
+//     return serverless(api)(event, context);
+//   }
+
+// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
+
 import express, { Router } from "express";
 import serverless from "serverless-http";
 
-export async function handler(event, context) {
-    const api = express();
-    const router = Router();
+const api = express();
 
-    router.get('/fulfilled', (req, res) => res.status(200).json({ "work": "Lets go"}));
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
 
-    api.use('/api/', router);
-  
-    return serverless(api)(event, context);
-  }
+api.use("/api/", router);
 
+export const handler = serverless(api);
