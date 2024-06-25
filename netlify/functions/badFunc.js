280144dc41f235218e318bd2ab7f7aa932309366
justsilencia <legalpet@protonmail.com>
2024-06-25 09:07:00 -0400

    Fix.

--- netlify/functions/badFunc.js
+++ netlify/functions/badFunc.js
@@ -1,27 +1,15 @@
-// import express, { Router } from "express";
-// import serverless from "serverless-http";
-
-// export async function handler(event, context) {
-//     const api = express();
-//     const router = Router();
-
-//     router.get('/fulfilled', (req, res) => res.status(200).json({ "work": "Lets go"}));
-
-//     api.use('/api/', router);
-  
-//     return serverless(api)(event, context);
-//   }
-
-// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
-
 import express, { Router } from "express";
 import serverless from "serverless-http";
 
-const api = express();
+export async function handler(event, context) {
+    const api = express();
+    const router = Router();
 
-const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
+    router.get('/fulfilled', (req, res) => res.status(200).json({ "work": "Lets go"}));
 
-api.use("/api/", router);
+    api.use('/api/', router);
+  
+    return serverless(api)(event, context);
+  }
 
-export const handler = serverless(api);
+YOUR_BASE_DIRECTORY/netlify/functions/api.ts
